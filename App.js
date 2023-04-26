bac45c0b4edf14172b0c6024074b33666ec0cf23
kiendao <kiendao@var-meta.com>
2023-04-26 22:24:24 +0700

    update

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-native-paper";
+import { theme } from "./src/core/theme";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import ResetPasswordScreen from "./src/screens/ResetPasswordScreen";
+import StartScreen from "./src/screens/StartScreen";
+import { LogBox } from 'react-native';
+// import {Statistic} from "./src/screens/Statistic"
+LogBox.ignoreLogs(['Non-serializable values were found in the navigation state', 'syncStorage has been extracted from react-native core and will be removed in a future release', 'Warning: Each child in a list should have a unique "key" prop']);
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <Provider theme={theme}>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="StartScreen"
+                    screenOptions={{
+                        headerShown: false,
+                    }}
+                >
+                    <Stack.Screen name="StartScreen" component={StartScreen} />
+                    <Stack.Screen name="LoginScreen" component={LoginScreen} />
+                    <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+                    {/* <Stack.Screen name="Statistic"
+                        screenOptions={{
+                            headerShown: false,
+                        }}
+                        component={Statistic} /> */}
+                    <Stack.Screen
+                        name="ResetPasswordScreen"
+                        component={ResetPasswordScreen}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
+    )
+}
